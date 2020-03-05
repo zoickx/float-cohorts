221a58982bdd6ab289a7b1ec35611c85d5d66220
Ilia Zaichuk <zoickx@ztld.org>
2020-03-05 10:20:06 +0200

    Remove [Proper] instances They were weak compared to [_equiv] lemmas

--- Cohorts.v
+++ Cohorts.v
@@ -43,27 +43,6 @@ Proof.
   reflexivity.
 Qed.
 
-Instance dec_e_proper :
-  Proper (equiv ==> equiv) dec_e.
-Proof.
-  intros fp1 fp2 EQ.
-  destruct fp1 as [m1 e1], fp2 as [m2 e2].
-  cbn in *.
-  destruct EQ as [EQ | EQ]; destruct EQ as [E M].
-  -
-    left.
-    split; [lia |].
-    replace (e1 - 1 - (e2 - 1)) with (e1 - e2) by lia.
-    destruct (2 ^ (e1 - e2)).
-    all: lia.
-  -
-    right.
-    split; [lia |].
-    replace (e2 - 1 - (e1 - 1)) with (e2 - e1) by lia.
-    destruct (2 ^ (e2 - e1)).
-    all: lia.
-Qed.
-
 Lemma inc_e_equiv (fp : float_pair) :
   is_Some (inc_e fp) ->
   inc_e fp === Some fp.
@@ -86,46 +65,6 @@ Proof.
   reflexivity.
 Qed.
 
-Instance inc_e_proper_if_Some :
-  Proper (equiv ==> equiv_if_Some) inc_e.
-Proof.
-  intros fp1 fp2 EQ S1 S2.
-  destruct fp1 as [m1 e1], fp2 as [m2 e2].
-  destruct EQ as [EQ | EQ]; destruct EQ as [E M].
-  -
-    cbn in *.
-    destruct (div2_opt m1) as [hm1|] eqn:HM1,
-             (div2_opt m2) as [hm2|] eqn:HM2.
-    all: try apply div2_opt_correct in HM1.
-    all: try apply div2_opt_correct in HM2.
-    all: subst; cbn in *.
-    all: try inversion S1.
-    all: try inversion S2.
-    constructor.
-    left.
-    cbn.
-    split; [lia |].
-    replace (e1 + 1 - (e2 + 1)) with (e1 - e2) by lia.
-    destruct (2 ^ (e1 - e2)).
-    all: lia.
-  -
-    cbn in *.
-    destruct (div2_opt m1) as [hm1|] eqn:HM1,
-             (div2_opt m2) as [hm2|] eqn:HM2.
-    all: try apply div2_opt_correct in HM1.
-    all: try apply div2_opt_correct in HM2.
-    all: subst; cbn in *.
-    all: try inversion S1.
-    all: try inversion S2.
-    constructor.
-    right.
-    cbn.
-    split; [lia |].
-    replace (e2 + 1 - (e1 + 1)) with (e2 - e1) by lia.
-    destruct (2 ^ (e2 - e1)).
-    all: lia.
-Qed.
-
 Lemma dec_e_by_equiv (fp : float_pair) (de : positive) :
   dec_e_by fp de === fp.
 Proof.
@@ -134,14 +73,6 @@ Proof.
   intros.
   rewrite <-H.
   apply dec_e_equiv.
-Qed.
-
-Instance dec_e_by_proper :
-  Proper (equiv ==> (fun _ _ => True) ==> equiv) dec_e_by.
-Proof.
-  intros fp1 fp2 FE d1 d2 DE.
-  repeat rewrite dec_e_by_equiv.
-  assumption.
 Qed.
 
 Lemma inc_e_by_equiv (fp : float_pair) (de : positive) :
@@ -159,15 +90,6 @@ Proof.
   constructor.
 Qed.
 
-Instance inc_e_by_proper_if_some :
-  Proper (equiv ==> equiv ==> equiv_if_Some) inc_e_by.
-Proof.
-  intros fp1 fp2 FPE de1 de2 DEE S1 S2.
-  repeat rewrite inc_e_by_equiv; try assumption.
-  rewrite FPE.
-  reflexivity.
-Qed.
-
 Lemma shift_e_equiv (fp : float_pair) (de : Z) :
   is_Some (shift_e fp de) ->
   shift_e fp de === Some fp.
@@ -180,29 +102,11 @@ Proof.
   - rewrite dec_e_by_equiv. reflexivity.
 Qed.
 
-Instance shift_e_proper_if_Some :
-  Proper (equiv ==> equiv ==> equiv_if_Some) shift_e.
-Proof.
-  intros fp1 fp2 FPE de1 de2 DEE S1 S2.
-  repeat rewrite shift_e_equiv; try assumption.
-  rewrite FPE.
-  reflexivity.
-Qed.
-
 Lemma set_e_equiv (fp : float_pair) (e : Z) :
   is_Some (set_e fp e) ->
   set_e fp e === Some fp.
 Proof.
   apply shift_e_equiv.
-Qed.
-
-Instance set_e_proper_if_Some :
-  Proper (equiv ==> equiv ==> equiv_if_Some) set_e.
-Proof.
-  intros fp1 fp2 FPE de1 de2 DEE S1 S2.
-  repeat rewrite set_e_equiv; try assumption.
-  rewrite FPE.
-  reflexivity.
 Qed.
 
 
@@ -225,62 +129,29 @@ Lemma inc_digits_m_equiv (fp : float_pair) :
   inc_digits_m fp === fp.
 Proof. apply dec_e_equiv. Qed.
 
-Lemma inc_digits_m_proper :
-  Proper (equiv ==> equiv) inc_digits_m.
-Proof. apply dec_e_proper. Qed.
-
 Lemma dec_digits_m_equiv (fp : float_pair) :
   is_Some (dec_digits_m fp) ->
   dec_digits_m fp === Some fp.
 Proof. apply inc_e_equiv. Qed.
 
-Instance dec_digits_m_proper_if_Some :
-  Proper (equiv ==> equiv_if_Some) dec_digits_m.
-Proof. apply inc_e_proper_if_Some. Qed.
-
 Lemma inc_digits_m_by_equiv (fp : float_pair) (ddm : positive) :
   inc_digits_m_by fp ddm === fp.
 Proof. apply dec_e_by_equiv. Qed.
-
-Lemma inc_digits_m_by_proper :
-  Proper (equiv ==> (fun _ _ => True) ==> equiv) inc_digits_m_by.
-Proof. apply dec_e_by_proper. Qed.
 
 Lemma dec_digits_m_by_equiv (fp : float_pair) (ddm : positive) :
   is_Some (dec_digits_m_by fp ddm) ->
   dec_digits_m_by fp ddm === Some fp.
 Proof. apply inc_e_by_equiv. Qed.
 
-Instance dec_digits_m_by_proper_if_some :
-  Proper (equiv ==> equiv ==> equiv_if_Some) dec_digits_m_by.
-Proof. apply inc_e_by_proper_if_some. Qed.
-
 Lemma shift_digits_m_equiv (fp : float_pair) (ddm : Z) :
   is_Some (shift_digits_m fp ddm) ->
   shift_digits_m fp ddm === Some fp.
 Proof. apply shift_e_equiv. Qed.
 
-Instance shift_digits_m_proper_if_Some :
-  Proper (equiv ==> equiv ==> equiv_if_Some) shift_digits_m.
-Proof.
-  intros fp1 fp2 FE ddm1 ddm2 DE.
-  apply shift_e_proper_if_Some.
-  assumption.
-  inversion DE; reflexivity.
-Qed.
 Lemma set_digits_m_equiv (fp : float_pair) (dm : positive) :
   is_Some (set_digits_m fp dm) ->
   set_digits_m fp dm === Some fp.
 Proof. apply shift_e_equiv. Qed.
-
-Instance set_digits_m_proper_if_Some :
-  Proper (equiv ==> equiv ==> equiv_if_Some) set_digits_m.
-Proof.
-  intros fp1 fp2 FE dm1 dm2 DE S1 S2.
-  unfold set_digits_m.
-  repeat rewrite shift_digits_m_equiv; try assumption.
-  rewrite FE; reflexivity.
-Qed.
 
 
 
@@ -730,7 +601,7 @@ Proof.
     apply inc_e_exact.
     symmetry; apply dec_e_equiv.
     rewrite dec_e_res; lia.
-    rewrite H; apply dec_e_equiv.
+    rewrite <-H; apply dec_e_equiv.
     rewrite dec_e_res; lia.
 Qed.
 
@@ -866,7 +737,7 @@ Proof.
     apply dec_digits_m_exact.
     symmetry; apply inc_digits_m_equiv.
     rewrite inc_digits_m_res; lia.
-    rewrite H; apply inc_digits_m_equiv.
+    rewrite <-H; apply inc_digits_m_equiv.
     rewrite inc_digits_m_res; lia.
 Qed.
 
